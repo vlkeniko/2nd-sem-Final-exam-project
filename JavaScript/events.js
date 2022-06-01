f919e724e6bf92c30c70283716334813a0d587ac
Giulia Ivan <eaagril@students.eaaa.dk>
2022-06-01 14:11:12 +0200

    Revert "Merge branch 'main' of https://github.com/vlkeniko/2nd-sem-Final-exam-project"
    This reverts commit ce35241c10fb6b09b299aebc70e47151b22e58bb, reversing
    changes made to fbd242d8629ec17b90e15745689e4b32ea35006e.

--- JavaScript/events.js
+++ JavaScript/events.js
@@ -1,7 +1,7 @@
-// Declare variables and select all the elements with the class "slide" & "dot"
 var slides = document.querySelectorAll(".slide");
 var dots = document.querySelectorAll(".dot");
 var index = 0;
+
 
 function prevSlide(n){
   index+=n;
@@ -17,22 +17,27 @@ function nextSlide(n){
 changeSlide();
 
 function changeSlide(){
-  
-  // if the index value is bigger than the slideshow nr. of images (length)
+    
   if(index>slides.length-1)
-  index=0; // then show the first image in the slideshow
-
-  // if the index value is less than the slideshow nr. of pages (length)
+    index=0;
+  
   if(index<0)
-  index=slides.length-1; // then show the last image in the slideshow
+    index=slides.length-1;
+  
+  
+  
+    for(let i=0;i<slides.length;i++){
+      slides[i].style.display = "none";
+      
+      dots[i].classList.remove("active");
+      
+      
+    }
+    
+    slides[index].style.display = "block";
+    dots[index].classList.add("active");
 
-  // as the value of "i" increases until it reaches the value of the slides length (nr. of images in the slideshow)
-  // then execute this "for"
-  for(let i=0;i<slides.length;i++){
-  slides[i].style.display = "none"; // hide the previous image
-  dots[i].classList.remove("active"); // remove the active status of the specific dot of the previous image
-  }
+  
 
-  slides[index].style.display = "block"; // show the actual image
-  dots[index].classList.add("active"); // add the active status of the specific dot of the actual image
 }
+
